83d97b917a9f8ca89412850709876739efe37c9b
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-07-24 23:19:50 +0200

    registration form handling

--- src/main/java/pl/coderslab/web/Login.java
+++ src/main/java/pl/coderslab/web/Login.java
@@ -1,5 +1,7 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +12,23 @@ import java.io.IOException;
 @WebServlet("/login") public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/login.jsp").forward(req,resp);
+        getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+       /*
+       tutaj będzie sprawdzanie metodą z klasy AdminDao
+
+       if (AdminDao.validation) {
+            getServletContext().getRequestDispatcher("/app/dashboard.jsp").forward(req, resp);
+        } else {
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+        */
+
     }
 }
