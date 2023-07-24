01f02bae48ef9abaecbc269103f26747a4f5b212
Łukasz N <lnawrocki84@gmail.com>
2023-07-24 19:58:20 +0200

    LandingPage

--- src/main/java/pl/coderslab/web/HomeServlet.java
+++ src/main/java/pl/coderslab/web/HomeServlet.java
@@ -1,18 +1,11 @@
 package pl.coderslab.web;
 
-import pl.coderslab.dao.BookDao;
-import pl.coderslab.dao.RecipeDao;
-import pl.coderslab.model.Book;
-import pl.coderslab.model.Recipe;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-
 
 
 /**
@@ -22,12 +15,6 @@ import java.util.List;
 public class HomeServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        BookDao bookDao = new BookDao();
-        List<Book> books = bookDao.findAll();
-        System.out.println(books);
-
-
-
 
         getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
     }
