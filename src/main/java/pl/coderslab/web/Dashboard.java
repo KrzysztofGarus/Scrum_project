292761be7a71387a54d9d1f9e33f10251b7dcfea
KrzysztofGarus <krzysztof.garus@gmail.com>
2023-07-30 11:11:58 +0200

    dashboard servlet

--- src/main/java/pl/coderslab/web/Dashboard.java
+++ src/main/java/pl/coderslab/web/Dashboard.java
@@ -1,16 +1,26 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/app/dashboard")
 public class Dashboard extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        int userId = Integer.parseInt(String.valueOf(session.getAttribute("userId")));
+        Admin admin = new Admin();
+        admin = AdminDao.read(userId);
+        req.setAttribute("adminName", admin.getFirstName());
+
         getServletContext().getRequestDispatcher("/dashboard.jsp").forward(req, resp);
     }
 }
