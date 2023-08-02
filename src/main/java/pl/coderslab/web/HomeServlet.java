2b1edec04a26ba725f082ce0ef103f4e79c350de
Łukasz N <lnawrocki84@gmail.com>
2023-08-02 18:45:59 +0200

    main page redirection loged / not loged link update

--- src/main/java/pl/coderslab/web/HomeServlet.java
+++ src/main/java/pl/coderslab/web/HomeServlet.java
@@ -1,5 +1,7 @@
 package pl.coderslab.web;
 
+import pl.coderslab.dao.AdminDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,7 +23,9 @@ public class HomeServlet extends HttpServlet {
         if (sess.getAttribute("userId") == null) {
             getServletContext().getRequestDispatcher("/home.jsp").forward(req, resp);
         } else {
-            getServletContext().getRequestDispatcher("/dashboard.jsp").forward(req, resp);
+
+
+            getServletContext().getRequestDispatcher("/app/dashboard").forward(req, resp);
         }
 
 
